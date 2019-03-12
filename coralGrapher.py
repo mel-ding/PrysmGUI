424cc8d10c77c98baa82ef77a747e0cd6ad0e488
Melinda Ding <mjd4@rice.edu>
2019-03-12 16:25:53 -0500

    Add downloadable options

--- coralGrapher.py
+++ coralGrapher.py
@@ -21,24 +21,31 @@ class Grapher(tk.Frame):
 	def __init__(self, parent, *args, **kwargs):
 		tk.Frame.__init__(self, parent, *args, **kwargs)
 		self.parent = parent
-		self.parent.title("Coral Grapher")
+		self.parent.title("Coral PRSYM Models")
+		scrollbar = tk.Scrollbar(root, orient="vertical")
+
+		# =========================================================================================
+		# SENSOR DATA
+		# =========================================================================================
 
 		# Title of Page 
-		label = tk.Label(root, text="Coral Graph Page", font=LARGE_FONT)
-		label.grid(padx=(25,0))
+		label = tk.Label(root, text="Coral Sensor Data", font=LARGE_FONT)
+		label.grid(sticky="E")
 
 		rowIdx = 1
 
-		# COEFFICIENT ENTRY BOXES =================================================================
+		# =========================================================================================
+		# COEFFICIENT ENTRY BOXES
+		# =========================================================================================
 
 		# Longitude 
-		tk.Label(root, text="Longitude [0, 360]").grid(row=rowIdx, column=0)
+		tk.Label(root, text="Longitude (Range 0 to 360)").grid(row=rowIdx, column=0, sticky="E")
 		self.lonEntry = tk.Entry(root)
 		self.lonEntry.grid(row=rowIdx, column=1)
 		rowIdx += 1
 
 		# Latitude 
-		tk.Label(root, text="Latitude [-90, 90]").grid(row=rowIdx, column=0)
+		tk.Label(root, text="Latitude (Range -90 to 90)").grid(row=rowIdx, column=0, sticky="E")
 		self.latEntry = tk.Entry(root)
 		self.latEntry.grid(row=rowIdx, column=1)
 		rowIdx += 1
@@ -49,41 +56,51 @@ class Grapher(tk.Frame):
 
 		self.v = tk.StringVar()
 		self.v.set("default")
-		tk.Label(root, text = "Pick a Coral Species:").grid(row=rowIdx, column=0)
+		tk.Label(root, text = "Pick a Coral Species:").grid(row=rowIdx, column=0, sticky="E")
 		for text in species: 
 			b = tk.Radiobutton(root, text=text, variable=self.v, value=text)
 			b.grid(row=rowIdx, column=1, sticky="w")
 			rowIdx+=1
 
-		# ERROR OPTIONS ==========================================================================
-
-		# TODO: ask if want checkboxes or radio buttons? 
-		# is showing it on the right okay?
+		# =========================================================================================
+		# ERROR OPTIONS
+		# =========================================================================================
 
 		# Age Uncertainties
 		self.ageErrorVal = tk.BooleanVar() 
 		self.ageErrorVar = tk.Checkbutton(root, variable=self.ageErrorVal)
 		self.ageErrorVar.grid(row=rowIdx, column=2)
-		tk.Label(root, text = "Add age uncertainties with error rate of:").grid(row=rowIdx, column=0)
+		tk.Label(root, text = "Add age uncertainties with error rate:").grid(row=rowIdx, column=0, sticky="E")
 		self.ageErrorEntry = tk.Entry(root)
 		self.ageErrorEntry.grid(row=rowIdx, column=1)
 		rowIdx += 1
 
-		# Analytical Errors
+		# Simple Analytical Errors
+		self.simAltErrorVal = tk.BooleanVar() 
+		self.simAltErrorVar = tk.Checkbutton(root, variable=self.simAltErrorVal)
+		self.simAltErrorVar.grid(row=rowIdx, column=2)
+		tk.Label(root, text = "Add simple analytical error with precision:").grid(row=rowIdx, column=0, sticky="E")
+		self.simAltErrorEntry = tk.Entry(root)
+		self.simAltErrorEntry.grid(row=rowIdx, column=1)
+		rowIdx += 1
+
+		# Gaussian Analytical Errors
 		self.altErrorVal = tk.BooleanVar() 
 		self.altErrorVar = tk.Checkbutton(root, variable=self.altErrorVal)
 		self.altErrorVar.grid(row=rowIdx, column=2)
-		tk.Label(root, text = "Add simple analytical error with precision of:").grid(row=rowIdx, column=0)
+		tk.Label(root, text = "Add gaussian analytical error with precision:").grid(row=rowIdx, column=0, sticky="E")
 		self.altErrorEntry = tk.Entry(root)
 		self.altErrorEntry.grid(row=rowIdx, column=1)
 		rowIdx += 1
 
-		# GRAPHER AND BUTTONS ====================================================================
+		# =========================================================================================
+		# BUTTONS 
+		# =========================================================================================
 
 		# Creates example graph (as seen in paper)
-		tk.Label(root, text = "Click to see an example graph:").grid(row=rowIdx, column=0)
+		tk.Label(root, text = "Click to see an example graph:").grid(row=rowIdx, column=0, sticky="E")
 		exampleGraphButton = tk.Button(root, text="Example Graph", command=self.exampleGraph)
-		exampleGraphButton.grid(row=rowIdx, column=1)
+		exampleGraphButton.grid(row=rowIdx, column=1, ipadx=20, ipady=3, sticky="W")
 		rowIdx+=1 
 
 		# Allows user to upload data. 
@@ -91,32 +108,77 @@ class Grapher(tk.Frame):
 			text="Upload a single CSV file with three columns \n and the headers \"TIME\", \"SST\", \"SSS\"."
 			).grid(row=rowIdx, columnspan=3, rowspan=3)
 		rowIdx += 3
-		tk.Label(root, text = "Click to upload your data:").grid(row=rowIdx, column=0)
+		tk.Label(root, text = "Click to upload your data:").grid(row=rowIdx, column=0, sticky="E")
 		graphButton = tk.Button(root, text="Upload Data", command=self.uploadData)
-		graphButton.grid(row=rowIdx, column=1)
+		graphButton.grid(row=rowIdx, column=1, ipadx=30, ipady=3, sticky="W")
 		rowIdx += 1
 
 		# Shows the name of the current uploaded file, if any. 
-		tk.Label(root, text="Current File Uploaded:").grid(row=rowIdx, column=0)
+		tk.Label(root, text="Current File Uploaded:").grid(row=rowIdx, column=0, sticky="E")
 		self.currentFileLabel = tk.Label(root, text="No file")
-		self.currentFileLabel.grid(row=rowIdx, column=1, columnspan=2)
+		self.currentFileLabel.grid(row=rowIdx, column=1, columnspan=2, sticky="W")
 		rowIdx+=1
 
 		# Creates graph with given inputs
-		tk.Label(root, text = "Click to create your own graph:").grid(row=rowIdx, column=0)
+		tk.Label(root, text = "Click to create your own graph:").grid(row=rowIdx, column=0, sticky="E")
 		graphButton = tk.Button(root, text="Generate Graph", command=self.generateGraph)
-		graphButton.grid(row=rowIdx, column=1)
+		graphButton.grid(row=rowIdx, column=1, ipadx=20, ipady=3, sticky="W")
 		rowIdx+=1
 
 		# Clears graph
-		tk.Label(root, text = "Click to clear the graph:").grid(row=rowIdx, column=0)
-		clearButton = tk.Button(root, text="Clear", command=self.clear)
-		clearButton.grid(row=rowIdx, column=1)
+		tk.Label(root, text = "Click to clear the graph:").grid(row=rowIdx, column=0, sticky="E")
+		clearGraphButton = tk.Button(root, text="Clear Graph", command=self.clearGraph)
+		clearGraphButton.grid(row=rowIdx, column=1, ipadx=33, ipady=3, sticky="W")
+		rowIdx+=1
 
-		f = Figure(figsize=(10,5), dpi=100)
-		self.plt = f.add_subplot(111)
-		self.canvas = FigureCanvasTkAgg(f, root)
+		# Clears entries
+		tk.Label(root, text = "Click to clear the entries:").grid(row=rowIdx, column=0, sticky="E")
+		clearEntryButton = tk.Button(root, text="Clear Entries", command=self.clearEntries)
+		clearEntryButton.grid(row=rowIdx, column=1, ipadx=30, ipady=3, sticky="W")
+		rowIdx+=1 
+
+		# =========================================================================================
+		# SAVE OPTIONS 
+		# =========================================================================================	
+		tk.Label(root, text = "Save coral data as:").grid(row=19, column=6, sticky="E")
+		dataTxtbutton = tk.Button(root, text=".txt", command=self.saveTxtData)
+		dataTxtbutton.grid(row=19, column=8, ipadx=20, ipady=3, sticky="W")
+		dataNpybutton = tk.Button(root, text=".npy", command=self.saveNpyData)
+		dataNpybutton.grid(row=19, column=9, ipadx=20, ipady=3, sticky="W")
+
+		tk.Label(root, text = "Save graph as:").grid(row=20, column=6, sticky="E")
+		dataTxtbutton = tk.Button(root, text=".png", command=self.savePngGraph)
+		dataTxtbutton.grid(row=20, column=8, ipadx=20, ipady=3, sticky="W")
+
+		# =========================================================================================
+		# GRAPH
+		# =========================================================================================
+		self.f = Figure(figsize=(10,5), dpi=100)
+		self.plt = self.f.add_subplot(111)
+		self.canvas = FigureCanvasTkAgg(self.f, root)
 		self.canvas.get_tk_widget().grid(row=0, column=3, rowspan=16, columnspan=15, sticky="nw")
+
+	"""
+	Saves current graph.
+	"""
+	def savePngGraph(self):
+		self.f.savefig('test.png')
+		tk.messagebox.showinfo("Sucess", "Saved graph as graph.png")
+
+	"""
+	Saves the coral data into a numpy file. 
+	"""
+	def saveNpyData(self):
+		np.save("simulated_coral_d18O.npy", self.coral)
+		tk.messagebox.showinfo("Sucess", "Saved simulated data as 'simulated_coral_d18O.npy'")
+
+	"""
+	Saves the coral data into a text file. 
+	"""
+	def saveTxtData(self):
+		# np.save("simulated_coral_d18O.npy", self.coral)
+		np.savetxt("simulated_coral_d18O.txt", self.coral, newline=" ")
+		tk.messagebox.showinfo("Sucess", "Saved simulated data as 'simulated_coral_d18O.txt'")
 
 	"""
 	Creates example graph as seen in paper. 
@@ -127,8 +189,8 @@ class Grapher(tk.Frame):
 		# Time values 
 		self.time = np.arange(850,1850,1)
 		# Get y-values from driver script 
-		coral = np.load('coral/simulated_coral_d18O.npy')
-		# Plot age uncertainties - if selected 
+		self.coral = np.load('coral/simulated_coral_d18O.npy')
+		# Plot age uncertainties, if selected 
 		if (self.ageErrorVal.get()):
 			# Get the input error rate value
 			rateRaw = self.ageErrorEntry.get()
@@ -137,15 +199,37 @@ class Grapher(tk.Frame):
 				return
 			rate = float(rateRaw)
 			# Calculate the age uncertanties
-			X = coral
+			X = self.coral
 			X = X.reshape(len(X),1)
 			tp, Xp, tmc = banded.bam_simul_perturb(X, self.time, param=[rate, rate])
 			q1=mquantiles(Xp,prob=[0.025,0.975],axis=1)
 			q2=self.time
 			self.plt.fill_between(q2,q1[:,0],q1[:,1],label='1000 Age-Perturbed Realizations, CI',facecolor='gray',alpha=0.5)
 
+		# Plot gaussian analytical errors - if selected
+		if (self.altErrorVal.get()):
+			# Get the input error rate value
+			sigmaRaw = self.altErrorEntry.get()
+			if sigmaRaw == "":
+				tk.messagebox.showerror("Error", "Invalid Rate Value")
+				return
+			sigma = float(sigmaRaw)
+			X = self.coral
+			X = X.reshape(len(X),1)
+			Xn = analytical_error.analytical_error(X, sigma, 1000)
+			print(Xn.shape) # (1000, 1000, 1000)
+			q1 = mquantiles(Xn,prob=[0.025,0.975],axis=1)
+			q2 = self.time
+			# q1 = Xn[:, [0]] # (1000, 1, 100)
+			# q2 = Xn[:, [1]] # (1000, 1, 100)
+			print(q1.shape)
+			self.plt.fill_between(self.time,q1,q2,label='100 Analytical Error Realizations, CI', 
+				facecolor='darkgray',alpha=0.5)
+			# self.plt.fill_between(q2,q1[:,0],q1[:,1],label='100 Gaussian Analytical Error Realizations, CI', 
+			# 	facecolor='yellow',alpha=0.5)
+
 		# Plot the graph
-		self.plt.plot(self.time, coral)
+		self.plt.plot(self.time, self.coral)
 		self.plt.set_xlabel('Time')
 		self.plt.set_ylabel('Simulated Coral Data')
 		self.canvas.draw()
@@ -211,50 +295,61 @@ class Grapher(tk.Frame):
 				self.SST[i] = self.SST[i]-274.15
 
 		# Fill coral array with data same size as input vectors.
-		coral = np.zeros(len(self.time))
+		self.coral = np.zeros(len(self.time))
 		for i in range(len(self.time)):
-			coral[i] = sensor.pseudocoral(lat,lon,self.SST[i],self.SSS[i],species=speciesInput)
+			self.coral[i] = sensor.pseudocoral(lat,lon,self.SST[i],self.SSS[i],species=speciesInput)
+
+		# Reshape coral data for uncertainty calculations 
+		X = self.coral
+		X = X.reshape(len(X),1)
 
 		# Plot age uncertainties - if selected 
 		if (self.ageErrorVal.get()):
-			# Get the input error rate value
+			# Get input error rate
 			rateRaw = self.ageErrorEntry.get()
 			if rateRaw == "":
 				tk.messagebox.showerror("Error", "Invalid Rate Value")
 				return
 			rate = float(rateRaw)
 			# Calculate the age uncertanties
-			X = coral
-			X = X.reshape(len(X),1)
 			tp, Xp, tmc = banded.bam_simul_perturb(X, self.time, param=[rate, rate])
 			q1=mquantiles(Xp,prob=[0.025,0.975],axis=1)
 			q2=self.time
-			self.plt.fill_between(q2,q1[:,0],q1[:,1],label='1000 Age-Perturbed Realizations, CI',facecolor='gray',alpha=0.5)
+			# Graph quantiles 
+			self.plt.fill_between(q2,q1[:,0],q1[:,1],label='1000 Age-Perturbed Realizations, CI', 
+				facecolor='gray',alpha=0.5)
 
-		# Plot analytical errors - if selected
+		# Plot simple analytical errors - if selected
+		if (self.simAltErrorVal.get()):
+			# Get input error rate
+			sigmaRaw = self.simAltErrorEntry.get()
+			if sigmaRaw == "":
+				tk.messagebox.showerror("Error", "Invalid Rate Value")
+				return
+			sigma = float(sigmaRaw)
+			q1, q2 = analytical_err_simple.analytical_err_simple(X,sigma)
+			# Reshape quanties for graphing
+			q1 = q1.reshape(len(self.time))
+			q2 = q2.reshape(len(self.time))
+			# Graph quantiles 
+			self.plt.fill_between(self.time,q1,q2,label='100 Analytical Error Realizations, CI', 
+				facecolor='darkgray',alpha=0.5)
+
+		# Plot gaussian analytical errors - if selected
 		if (self.altErrorVal.get()):
-			# Get the input error rate value
+			# Get input error rate
 			sigmaRaw = self.altErrorEntry.get()
 			if sigmaRaw == "":
 				tk.messagebox.showerror("Error", "Invalid Rate Value")
 				return
 			sigma = float(sigmaRaw)
-			X = coral
-			X = X.reshape(len(X),1)
-			q1, q2 = analytical_err_simple.analytical_err_simple(X,sigma)
-			q1 = q1.reshape(len(self.time))
-			q2 = q2.reshape(len(self.time))
-			self.plt.fill_between(self.time,q1,q2,label='100 Analytical Error Realizations, CI',facecolor='darkgray',alpha=0.5)
-
-		"""
-		for analytical errors need to use quantile
-		q1=mquantiles(Xp,prob=[0.025,0.975],axis=1)
-		q2=self.time
-
-		add error rates to clear function 
-
-		download options - data and figure 
-		"""
+			# Get error values 
+			q1, q2 = analytical_error.analytical_error(X,sigma, 100)
+			q1 = mquantiles(Xp,prob=[0.025,0.975],axis=1)
+			q2 = self.time
+			# Graph quantiles 
+			self.plt.fill_between(q2,q1[:,0],q1[:,1],label='100 Analytical Error Realizations, CI', 
+				facecolor='yellow',alpha=0.5)
 
 		# Plot the graph
 		self.plt.plot(self.time, coral)
@@ -263,15 +358,25 @@ class Grapher(tk.Frame):
 		self.canvas.draw()
 
 	"""
-	Clears the content in the graph. 
-	TODO: Make clear button clear entry field contents
+	Clears all content in the graph. 
 	"""
-	def clear(self):
-		self.lonEntry.delete(0, tk.END)
-		self.latEntry.delete(0, tk.END)
-		self.v.set(None)
+	def clearGraph(self):
 		self.plt.clear()
 		self.canvas.draw()
+
+	"""
+	Clears entries the user has input. 
+	"""
+	def clearEntries(self):
+		self.ageErrorVal.set(0)
+		self.simAltErrorVal.set(0)
+		self.altErrorVal.set(0)
+		self.ageErrorEntry.delete(0, tk.END)
+		self.altErrorEntry.delete(0, tk.END)
+		self.lonEntry.delete(0, tk.END)
+		self.latEntry.delete(0, tk.END)		
+		self.v.set(None)
+
 
 """
 Initialize Application.
@@ -279,5 +384,6 @@ Initialize Application.
 if __name__ == "__main__":
     root = tk.Tk()
     Grapher(root)
-    root.geometry("1440x620+5+100")
+    root.geometry("1440x650+5+100")
     root.mainloop()
+
